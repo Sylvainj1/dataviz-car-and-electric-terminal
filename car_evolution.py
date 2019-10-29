8ab24ecd3c957cecdb38b990ebc7d34cb823343a
jeremys <jeremysurget77@gmail.com>
2019-10-29 13:18:59 +0100

    store dataframe in a variable to avoid recreating it every time

--- car_evolution.py
+++ car_evolution.py
@@ -2,18 +2,24 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-file ="nombre_total_de_points_de_charge.csv"
-borne = pd.read_csv(file,sep=";", encoding="UTF-8")
-borne=borne.sort_values(["Trimestre"],ascending=True)
 
-x=borne["Trimestre"]
-yParticulier=borne["Particulier"]
-ySociete=borne["Société"]
-yPublic=borne["Accessible au public"]
+def construct_car_evolution():
+    file ="nombre_total_de_points_de_charge.csv"
+    borne = pd.read_csv(file,sep=";", encoding="UTF-8")
+    borne=borne.sort_values(["Trimestre"],ascending=True)
 
-fig = go.Figure(go.Bar(x =x, y=ySociete, name='Société'))
-fig.add_trace(go.Bar(x=x, y=yParticulier, name='Particulier'))
-fig.add_trace(go.Bar(x=x, y=yPublic, name='Accessible au public'))
+    x=borne["Trimestre"]
+    yParticulier=borne["Particulier"]
+    ySociete=borne["Société"]
+    yPublic=borne["Accessible au public"]
 
-fig.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'})
-fig.show()
+    fig = go.Figure(go.Bar(x =x, y=ySociete, name='Société'))
+    fig.add_trace(go.Bar(x=x, y=yParticulier, name='Particulier'))
+    fig.add_trace(go.Bar(x=x, y=yPublic, name='Accessible au public'))
+
+    fig.update_layout(barmode='stack', xaxis={'categoryorder':'category ascending'})
+    return fig
+
+if __name__ =="__main__":
+    figure = construct_car_evolution()
+    figure.show()
--- main.py
+++ main.py
@@ -14,84 +14,65 @@ import geopandas as gpd
 
 import json
 
+import car_evolution
+
 
 
 
 #geojson du tracé des routes principales francaises
+#le json de base avait des values inutilisées dans notre cas que nous avons supprimés
+#nous avons aussi réduit la précision des coordonnées, tout cela pour allèger la taille du geosjon
+#la taille est passé de 3,6 Mo à 1,3 Mo -> le chargement est un poil plus rapide
 with open("routes_compress_all.json") as geofile:
     geojson_layer = json.load(geofile)
 
 
 
-def create_df_borne(filename):
+#création des différentes dataframe necessaires
+borne_public = pd.read_csv("bornedata.csv",sep=';',na_values = 'nan', encoding="UTF-8")
+borne_public_dataframe = borne_public.dropna()
+
+lat = []
+lon = []
+df_tolist = borne_public_dataframe["Coordonnees"].tolist()
+
+for coord in df_tolist:
+    sp = coord.split(',')
+    lat.append(float(sp[0]))
+    lon.append(float(sp[1]))
+
+borne_public_dataframe["Ylatitude"] = lat
+borne_public_dataframe["Xlongitude"] = lon
+borne_public_dataframe = borne_public_dataframe.rename(columns={"Puissance délivrée":"puiss_max"})
+
+
+
+borne_tesla = pd.read_csv("irve_tesla.csv",sep=';', encoding="utf-8")
+borne_tesla_dataframe = borne_tesla.drop(columns="ID_station")
+borne_tesla_dataframe = borne_tesla_dataframe.rename(columns={"Xlatitude":"Ylatitude"})
+
+
+borne_ionity = pd.read_csv("irve_ionity.csv",sep=';',na_values = 'nan', encoding="UTF-8")
+borne_ionity_dataframe = borne_ionity.dropna()
+
+
+def select_dataframe(filename):
     if filename == "bornedata.csv":
-        df = pd.read_csv(filename,sep=';',na_values = 'nan', encoding="UTF-8")
-        borne_dataframe = df.dropna()
+        return borne_public_dataframe
 
-        lat = []
-        lon = []
-        df_tolist = borne_dataframe["Coordonnees"].tolist()
-
-        for coord in df_tolist:
-            sp = coord.split(',')
-            lat.append(float(sp[0]))
-            lon.append(float(sp[1]))
-
-        borne_dataframe["Ylatitude"] = lat
-        borne_dataframe["Xlongitude"] = lon
-        borne_dataframe = borne_dataframe.rename(columns={"Puissance délivrée":"puiss_max"})
-        return borne_dataframe
-    
     if filename == "irve_tesla.csv":
-        df = pd.read_csv(filename,sep=';', encoding="utf-8")
-        borne_dataframe = df.drop(columns="ID_station")
-        borne_dataframe = borne_dataframe.rename(columns={"Xlatitude":"Ylatitude"})
-        return borne_dataframe
-    
+        return borne_tesla_dataframe
+
     if filename == "irve_ionity.csv":
-        df = pd.read_csv(filename,sep=';',na_values = 'nan', encoding="UTF-8")
-        borne_dataframe = df.dropna()
-        return borne_dataframe
+        return borne_ionity_dataframe
 
 
-first_borne_data = create_df_borne("bornedata.csv")
 
+#figure vide car app.Callback est appelé à l'initialisation de l'appli et créer la figure
+#pas besoin donc de créer une figure entière ici
 fig = go.Figure()
-# fig.add_trace(
-#     go.Scattermapbox(
-#         lat=first_borne_data["Ylatitude"],
-#         lon=first_borne_data["Xlongitude"],
-#         mode="markers",
-#         marker=go.scattermapbox.Marker(
-#                 color=first_borne_data["puiss_max"],
-#                 size=4,
-#                 colorscale="Viridis",
-#                 reversescale=True,
-#                 colorbar=dict(
-#                     title = "Puissance de charge (Kw)",
-#                 ),
-#             ),
-#     )
-# )
-
-# fig.update_layout(
-#     mapbox=go.layout.Mapbox(
-#         bearing=0,
-#         center=go.layout.mapbox.Center(
-#             lat=46.227638,
-#             lon=2.213749
-#         ),
-#         zoom=5,
-#         layers=[]
-#     )
-# )
-# fig.update_layout(mapbox_style="dark", mapbox_accesstoken=token, height=700)
-# fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-
-
 
 app = dash.Dash(__name__)
-
 
 app.layout = html.Div(children=[
     html.H1(
@@ -118,8 +99,16 @@ app.layout = html.Div(children=[
     dcc.Graph(
         id='map',
         figure=fig
+    ),
+
+    dcc.Graph(
+        id='car_evolution',
+        figure=car_evolution.construct_car_evolution()
     )
 
+
+
+    #bug avec le loading
     # dcc.Loading(
     #     children=[
     #         dcc.Graph(
@@ -136,10 +125,7 @@ app.layout = html.Div(children=[
         [Input(component_id='select-charger-type', component_property='value'),
         Input(component_id ='routes-checkbox', component_property='value')]
     )
-
 def update_map_figure(input_value, route_show):
-    print(input_value)
-    print(route_show)
     route = route_show
     layer =[]
     if route:
@@ -153,7 +139,7 @@ def update_map_figure(input_value, route_show):
             )
         ]
 
-    borne_data = create_df_borne(input_value)
+    borne_data = select_dataframe(input_value)
     fig = go.Figure()
     fig.add_trace(
         go.Scattermapbox(
